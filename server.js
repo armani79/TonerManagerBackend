39258f338be7ee6d9f76aa2fd53cc1f403899a61
Arman <arman.mohiuddin79@myhunter.cuny.edu>
2025-12-01 20:22:28 -0500

    Adding POST endpoint

--- server.js
+++ server.js
@@ -4,8 +4,32 @@ import { PrismaClient } from "./generated/prisma/client.js";
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
    res.send("Hello! This is my first web server endpoint!");
+});
+
+// Post endpoint
+app.post("/toners", async (req, res) => {
+   const { model, color = "", printers = "", stock = 0 } = req.body;
+
+   if (!model || typeof model !== "string" || model.trim() === "") {
+      return res
+         .status(400)
+         .json({ error: "Model and stock are required fields." });
+   }
+
+   const toner = await prisma.toner.create({
+      data: {
+         model,
+         color,
+         printers,
+         stock,
+      },
+   });
+
+   res.status(201).json(toner);
 });
 
 // Create a toner GET endpoint that returns a hardcoded lists of toner
